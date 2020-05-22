b836757d0a3eab0f3b4c1ca3deeed0d6ea17c4d1
João Victor Pereir Santos <joaovictorsantoscpv@gmail.com>
2020-05-22 14:45:19 -0300

    add: component to section of apresentation

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import GlobalStyle from './styles/global';
+import Apresentation from './components/Apresentation';
 
 const App: React.FC = () => (
   <>
     <GlobalStyle />
-    <h1>Teste</h1>
+    <Apresentation />
   </>
 );
 
---
+++ src/components/Apresentation.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Apresentation: React.FC = () => <div>Apresentation</div>;
+
+export default Apresentation;
