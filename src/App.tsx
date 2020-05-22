d2c2fcf74e8003af448f2e2aeac68e63f9fd425c
João Victor Pereir Santos <joaovictorsantoscpv@gmail.com>
2020-05-22 14:39:48 -0300

    add: global style

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
+import GlobalStyle from './styles/global';
 
-function App() {
-  return <h1>Hello</h1>;
-}
+const App: React.FC = () => (
+  <>
+    <GlobalStyle />
+    <h1>Teste</h1>
+  </>
+);
 
 export default App;
---
+++ src/styles/global.ts
@@ -0,0 +1,27 @@
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+
+ *{
+    margin: 0;
+    padding: 0;
+    outline: 0;
+    box-sizing: border-box;
+ }
+
+ body {
+   color: #a8a8b3;
+   background-color: inherit;
+   margin: 0px;
+ }
+
+ body, input button {
+   font-family: 'Roboto', sans-serif;
+   font-size: 16px;
+ }
+
+ button {
+   cursor: pointer;
+ }
+
+`;
