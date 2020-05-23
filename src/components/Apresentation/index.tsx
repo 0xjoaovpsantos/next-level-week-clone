6441e8d42b77820f3433e27d525fbc83725c77ea
João Victor Pereir Santos <joaovictorsantoscpv@gmail.com>
2020-05-23 11:48:27 -0300

    add: component Information within component Content and make it responsive

--- src/components/Apresentation/index.tsx
+++ src/components/Apresentation/index.tsx
@@ -2,10 +2,62 @@ import React from 'react';
 
 import { Container, Content, Information } from './styles';
 
+import logoImg from '../../assets/logo.svg';
+
 const Apresentation: React.FC = () => (
   <Container>
     <Content>
-      <Information></Information>
+      <Information>
+        <div>
+          <div>
+            <img src={logoImg} alt="Logo da Next Level Week" />
+            <div>
+              <div>
+                <svg
+                  stroke="currentColor"
+                  fill="none"
+                  stroke-width="2"
+                  viewBox="0 0 24 24"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  height="1em"
+                  width="1em"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <rect x="3" y="4" width="18" height="18" rx="2" ry="2"></rect>
+                  <line x1="16" y1="2" x2="16" y2="6"></line>
+                  <line x1="8" y1="2" x2="8" y2="6"></line>
+                  <line x1="3" y1="10" x2="21" y2="10"></line>
+                </svg>
+                <p>1 a 7 de Junho</p>
+              </div>
+              <div>
+                <svg
+                  stroke="currentColor"
+                  fill="none"
+                  stroke-width="2"
+                  viewBox="0 0 24 24"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  height="1em"
+                  width="1em"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <circle cx="12" cy="12" r="10"></circle>
+                  <line x1="12" y1="8" x2="12" y2="12"></line>
+                  <line x1="12" y1="16" x2="12" y2="16"></line>
+                </svg>
+                <p>100% online e gratuito</p>
+              </div>
+            </div>
+          </div>
+          <h1>
+            Uma semana para acelerar sua evolução como dev e avançar para o
+            próximo nível.
+          </h1>
+          <button type="button">Inscrição gratuita</button>
+        </div>
+      </Information>
     </Content>
   </Container>
 );
--- src/components/Apresentation/styles.ts
+++ src/components/Apresentation/styles.ts
@@ -14,8 +14,9 @@ export const Content = styled.div`
   max-width: 1160px;
   margin: 0 auto;
   padding: 0 20px;
-
   position: relative;
+  z-index: 2;
+  display: flex;
 
   &::after {
     content: '';
@@ -24,8 +25,8 @@ export const Content = styled.div`
     height: 872px;
     top: -23%;
     right: -14%;
-    transition: height 0.4s;
-    transition: width 0.4s;
+    transition: all 0.2s;
+    z-index: -1;
     background: url(${backgroundImage}) no-repeat;
   }
 
@@ -43,4 +44,105 @@ export const Content = styled.div`
 export const Information = styled.div`
   max-width: 620px;
   width: 100%;
+  display: flex;
+  padding-top: 40px;
+  @media only screen and (max-width: 960px) {
+    margin: 0 auto;
+  }
+
+  > div > h1 {
+    margin-top: 80px;
+    font-size: 36px;
+    color: #e1e1e6;
+    max-width: 580px;
+    margin-bottom: 40px;
+
+    @media only screen and (max-width: 960px) {
+      margin-top: 40px;
+      font-size: 28px;
+    }
+  }
+
+  > div > button {
+    font-size: 18px;
+    line-height: 72px;
+    max-width: 312px;
+    width: 100%;
+    background-color: #7159c1;
+    color: #e1e1e6;
+    border-radius: 5px;
+    font-weight: 700;
+    border: 0;
+    transition: all 0.2s;
+    box-shadow: 0px 6px 0px rgba(113, 89, 193, 0.6);
+    text-transform: uppercase;
+
+    @media only screen and (max-width: 960px) {
+      max-width: 100%;
+    }
+
+    @media only screen and (max-width: 640px) {
+      font-size: 15px;
+    }
+  }
+
+  > div > div {
+    display: flex;
+    align-items: center;
+
+    @media only screen and (max-width: 960px) {
+      flex-direction: column;
+      align-items: start;
+    }
+  }
+
+  @media only screen and (max-width: 960px) {
+    > div > div > img {
+      max-width: 117px;
+      margin-bottom: 40px;
+    }
+  }
+
+  > div > div > div {
+    display: flex;
+    margin-left: 96px;
+
+    @media only screen and (max-width: 960px) {
+      margin-left: 0;
+      align-items: center;
+    }
+  }
+
+  > div > div > div > div {
+    max-width: 140px;
+
+    @media only screen and (max-width: 960px) {
+      display: flex;
+      max-width: 123px;
+      width: 100%;
+    }
+
+    @media only screen and (max-width: 640px) {
+      font-size: 15px;
+    }
+  }
+
+  > div > div > div > div > svg {
+    width: 40px;
+    height: 40px;
+    color: #04d361;
+  }
+
+  > div > div > div > div > p {
+    font-size: 24px;
+    margin-top: 24px;
+    margin-left: 0;
+    color: #fff;
+
+    @media only screen and (max-width: 640px) {
+      font-size: 15px;
+      margin-top: 0;
+      margin-left: 16px;
+    }
+  }
 `;
