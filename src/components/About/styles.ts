479a1245c35e7a91a4252b226eda689a983f5c1f
João Victor Pereir Santos <joaovictorsantoscpv@gmail.com>
2020-05-24 14:08:06 -0300

    add: align text with img in component About

--- src/components/About/styles.ts
+++ src/components/About/styles.ts
@@ -10,4 +10,27 @@ export const Content = styled.div`
   width: 100%;
   margin: 0 auto;
   padding: 0 20px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  div {
+    margin-top: 120px;
+    max-width: 520px;
+  }
+
+  div h2 {
+    color: #e1e1e6;
+    font-size: 48px;
+    max-width: 382px;
+    margin-bottom: 40px;
+  }
+
+  div p {
+    line-height: 1.5;
+  }
+
+  p:first-child {
+    color: #04d361;
+  }
 `;
