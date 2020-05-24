8863b5f51a806d5dcfd211ab7ce526aa16ba52cd
João Victor Pereir Santos <joaovictorsantoscpv@gmail.com>
2020-05-24 13:50:16 -0300

    add: strcuture of About whitout style

--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -1,5 +1,31 @@
 import React from 'react';
 
-const About: React.FC = () => <h1>About</h1>;
+import { Container, Content } from './styles';
+
+import imgAbout from '../../assets/about.svg';
+
+const About: React.FC = () => (
+  <Container>
+    <Content>
+      <img src={imgAbout} alt="O que é a Next Level Week?" />
+      <div>
+        <h2>O que é o Next Level Week?</h2>
+        <p>
+          O NLW é uma semana prática com muito código, desafios, networking e um
+          único objetivo: te levar para o próximo nível.
+        </p>
+        <p>
+          Através do nosso método você vai aprender novas ferramentas, conhecer
+          novas tecnologias e descobrir hacks que vão impulsionar a sua
+          carreira.
+        </p>
+        <p>
+          Um evento online e totalmente gratuito que vai te ajudar a dar o
+          próximo passo na sua evolução como dev.
+        </p>
+      </div>
+    </Content>
+  </Container>
+);
 
 export default About;
---
+++ src/components/About/styles.ts
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const Container = styled.section`
+  background-color: #121616;
+  margin-top: 145px;
+`;
+
+export const Content = styled.div`
+  max-width: 1160px;
+  width: 100%;
+  margin: 0 auto;
+  padding: 0 20px;
+`;
