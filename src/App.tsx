f568a937553645030779cb4eacecfd9f15803bcf
João Victor Pereir Santos <joaovictorsantoscpv@gmail.com>
2020-05-24 15:05:54 -0300

    add: create component Methods

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import GlobalStyle from './styles/global';
 
 import Apresentation from './components/Apresentation/';
 import About from './components/About';
+import Methods from './components/Methods';
 
 const App: React.FC = () => (
   <>
     <GlobalStyle />
     <Apresentation />
     <About />
+    <Methods />
   </>
 );
 
---
+++ src/components/Methods/index.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Methods: React.FC = () => <h1>Methods</h1>;
+
+export default Methods;
