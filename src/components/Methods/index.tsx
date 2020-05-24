a5b5797e9c9badaf22b10ca574098b74aacc9d70
João Victor Pereir Santos <joaovictorsantoscpv@gmail.com>
2020-05-24 15:19:55 -0300

    add: create structure of Methods without style

--- src/components/Methods/index.tsx
+++ src/components/Methods/index.tsx
@@ -1,5 +1,45 @@
 import React from 'react';
 
-const Methods: React.FC = () => <h1>Methods</h1>;
+import { Container, Content } from './styles';
+
+import arrowsImg from '../../assets/arrows.svg';
+
+const Methods: React.FC = () => (
+  <Container>
+    <Content>
+      <img src={arrowsImg} alt="Arrows" />
+      <h2>Nosso método é baseado em 3 pilares</h2>
+      <div>
+        <div>
+          <h3>Prática</h3>
+          <p>
+            Existem muitas formas de estudar programação, mas só existe uma
+            forma de aprender: na prática. A teoria só funciona se você souber
+            onde e quando aplicar, e são as horas de código que vão te ensinar a
+            tomar as melhores decisões.
+          </p>
+        </div>
+        <div>
+          <h3>Foco</h3>
+          <p>
+            Programação é um universo infinito, por isso ter foco é o primeiro
+            passo para atingir seus objetivos. Saber onde você quer chegar e não
+            se distrair com cada ferramenta que surge é a melhor forma de
+            acelerar sua evolução.
+          </p>
+        </div>
+        <div>
+          <h3>Grupo</h3>
+          <p>
+            Para se destacar é preciso ter habilidades que vão além do código, e
+            a melhor forma de dominar essas habilidades também é na prática.
+            Ajudar outros devs, se posicionar e receber feedback formam o bom
+            profissional.
+          </p>
+        </div>
+      </div>
+    </Content>
+  </Container>
+);
 
 export default Methods;
---
+++ src/components/Methods/styles.ts
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  background-color: #121214;
+  margin-top: 253px;
+  margin-bottom: 320px;
+`;
+
+export const Content = styled.div`
+  max-width: 1160;
+  width: 100%;
+  margin: 0 auto;
+  padding: 0 20px;
+  display: flex;
+`;
