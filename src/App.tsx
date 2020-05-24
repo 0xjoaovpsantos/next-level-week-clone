d2df86f7ccec1bb6cd0a6cab2d1fad8ee4165986
João Victor Pereir Santos <joaovictorsantoscpv@gmail.com>
2020-05-24 13:37:06 -0300

    add: component About

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import GlobalStyle from './styles/global';
+
 import Apresentation from './components/Apresentation/';
+import About from './components/About';
 
 const App: React.FC = () => (
   <>
     <GlobalStyle />
     <Apresentation />
+    <About />
   </>
 );
 
---
+++ src/components/About/index.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const About: React.FC = () => <h1>About</h1>;
+
+export default About;
