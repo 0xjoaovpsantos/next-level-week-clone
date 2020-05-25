3bb6d99b0e4d8dd9b70570a0167511eb9db8e130
João Victor Pereir Santos <joaovictorsantoscpv@gmail.com>
2020-05-25 10:16:58 -0300

    add: structure of Pillars without style

--- src/components/Pillas/index.tsx
+++ src/components/Pillas/index.tsx
@@ -1,5 +1,53 @@
 import React from 'react';
 
-const Pillars: React.FC = () => <h1>Pillars</h1>;
+import { Container, Content } from './styles';
+
+const Pillars: React.FC = () => (
+  <Container>
+    <Content>
+      <div>
+        <h2>O poder dos 3 pilares combinados</h2>
+        <div>
+          <h3>
+            <span>01</span>
+            Experiência e portfolio
+          </h3>
+          <h3>
+            <span>02</span>
+            Confiança no seu código
+          </h3>
+          <h3>
+            <span>03</span>
+            Melhores <br /> decisões
+          </h3>
+          <h3>
+            <span>04</span>
+            Domínio das ferramentas
+          </h3>
+          <h3>
+            <span>05</span>
+            Velocidade de aprendizado
+          </h3>
+          <h3>
+            <span>06</span>
+            Maturidade profissional
+          </h3>
+          <h3>
+            <span>07</span>
+            Networking eficiente
+          </h3>
+          <h3>
+            <span>08</span>
+            Carreira <br /> acelerada
+          </h3>
+          <h3>
+            <span>09</span>
+            Constante <br /> evolução
+          </h3>
+        </div>
+      </div>
+    </Content>
+  </Container>
+);
 
 export default Pillars;
