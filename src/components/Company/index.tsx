240ddec5ecd95a9c701e92ad47c7fe8c6cd5b770
João Victor Pereir Santos <joaovictorsantoscpv@gmail.com>
2020-05-25 15:40:18 -0300

    add: create structure of Company without style

--- src/components/Company/index.tsx
+++ src/components/Company/index.tsx
@@ -1,5 +1,36 @@
 import React from 'react';
 
-const Company: React.FC = () => <h1>Company</h1>;
+import { Container, Content } from './styles';
+
+import companyLogo from '../../assets/rocketseat.svg';
+
+const Company: React.FC = () => (
+  <Container>
+    <Content>
+      <div>
+        <h2>Sobre a Rocketseat</h2>
+        <img src={companyLogo} alt="Rocketseat logo" />
+      </div>
+      <div>
+        <div>
+          <h3>8 mil</h3>
+          <p>Alunos treinados nos bootcamps</p>
+        </div>
+        <div>
+          <h3>173 mil</h3>
+          <p>Alunos iniciados nos cursos gratuitos</p>
+        </div>
+        <div>
+          <h3>50 mil</h3>
+          <p>Membros na comunidade aberta</p>
+        </div>
+        <div>
+          <h3>330 mil</h3>
+          <p>Devs impactados no Brasil</p>
+        </div>
+      </div>
+    </Content>
+  </Container>
+);
 
 export default Company;
