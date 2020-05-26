0ac5ab6b2fec3fa4cf5456779706e0cc5b627fe3
João Victor Pereir Santos <joaovictorsantoscpv@gmail.com>
2020-05-26 09:56:47 -0300

    add: make component Guests responsive

--- src/components/Guests/styles.ts
+++ src/components/Guests/styles.ts
@@ -15,21 +15,40 @@ export const Content = styled.div`
   align-items: center;
   justify-content: space-between;
 
+  @media only screen and (max-width: 960px) {
+    flex-direction: column;
+  }
+
   > img {
     max-width: 580px;
     min-width: 0;
     width: 100%;
     height: 100%;
     margin-right: 64px;
+    order: 1;
+
+    @media only screen and (max-width: 960px) {
+      order: 2;
+      margin-right: 0px;
+      margin-top: 40px;
+    }
   }
 
   div {
     display: flex;
     flex-direction: column;
+    order: 2;
+    @media only screen and (max-width: 960px) {
+      order: 1;
+    }
 
     h2 {
       font-size: 48px;
       color: #e1e1e6;
+
+      @media only screen and (max-width: 960px) {
+        font-size: 32px;
+      }
 
       b {
         color: #04d361;
@@ -41,6 +60,10 @@ export const Content = styled.div`
       min-width: 0;
       margin-left: auto;
       margin-top: 40px;
+
+      @media only screen and (max-width: 960px) {
+        display: none;
+      }
     }
   }
 `;
