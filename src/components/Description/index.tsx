72dfa1aefe903ba3e8e49b75501654b7293d7f20
João Victor Pereir Santos <joaovictorsantoscpv@gmail.com>
2020-05-26 11:23:56 -0300

    add: stylize structure of Description

--- src/components/Description/index.tsx
+++ src/components/Description/index.tsx
@@ -1,13 +1,20 @@
 import React from 'react';
 
-import { Container, Content } from './styles';
-
-import icon3Img from '../../assets/icon3.svg';
+import {
+  Container,
+  Content,
+  Information,
+  StepByStep,
+  Step,
+  StepOne,
+  StepTwo,
+  StepThree,
+} from './styles';
 
 const Description: React.FC = () => (
   <Container>
     <Content>
-      <div>
+      <Information>
         <div>
           <h2>Mas afinal, o que é o próximo nível?</h2>
           <p>
@@ -23,62 +30,68 @@ const Description: React.FC = () => (
             <b>o aprendizado é contínuo e sempre haverá um próximo nível.</b>
           </p>
         </div>
-      </div>
-      <div>
-        <div>
-          <h3>
-            <span>01</span>
-            Legal
-            <br />e o que é um dev?
-          </h3>
-          <p>
-            É a pessoa responsável por transformar linhas de código em soluções
-            que tornam a vida das pessoas mais fácil, como um aplicativo de
-            transporte por exemplo.
-          </p>
-          <p>
-            <b>Dev é a abreviação de developer</b>: uma palavra que não tem
-            gênero. Ou seja, um dev pode ser tanto um programador quanto uma
-            programadora.
-          </p>
-        </div>
-        <div>
-          <h3>
-            <span>02</span>
-            E esse evento
-            <br />é pra mim?
-          </h3>
-          <p>
-            Nessa edição o foco do NLW será o universo JavaScript: a tecnologia
-            essencial para todos os devs que trabalham ou pretendem trabalhar
-            com web.
-          </p>
-          <p>
-            <b>Se você é apaixonado por código</b>, está em busca de evolução e
-            não se cansa de aprender coisas novas todos os dias, esse evento é
-            para você.
-          </p>
-        </div>
-        <div>
-          <h3>
-            <span>03</span>
-            O que eu vou levar
-            <br />
-            desse evento?
-          </h3>
-          <p>
-            Aprendizado prático de novas ferramentas, hacks para sua carreira e
-            um trabalho incrível para colocar no seu portfólio.
-          </p>
-          <p>
-            Lembre que certificados não acrescentam nada na sua carreira,
-            <b>
-              a coisa mais importante que você pode levar daqui é o
-              conhecimento.
-            </b>
-          </p>
-        </div>
-      </div>
+      </Information>
+      <StepByStep>
+        <StepOne>
+          <Step>
+            <h3>
+              <span>01</span>
+              Legal
+              <br />e o que é um dev?
+            </h3>
+            <p>
+              É a pessoa responsável por transformar linhas de código em
+              soluções que tornam a vida das pessoas mais fácil, como um
+              aplicativo de transporte por exemplo.
+            </p>
+            <p>
+              <b>Dev é a abreviação de developer</b>: uma palavra que não tem
+              gênero. Ou seja, um dev pode ser tanto um programador quanto uma
+              programadora.
+            </p>
+          </Step>
+        </StepOne>
+        <StepTwo>
+          <Step>
+            <h3>
+              <span>02</span>
+              E esse evento
+              <br />é pra mim?
+            </h3>
+            <p>
+              Nessa edição o foco do NLW será o universo JavaScript: a
+              tecnologia essencial para todos os devs que trabalham ou pretendem
+              trabalhar com web.
+            </p>
+            <p>
+              <b>Se você é apaixonado por código</b>, está em busca de evolução
+              e não se cansa de aprender coisas novas todos os dias, esse evento
+              é para você.
+            </p>
+          </Step>
+        </StepTwo>
+        <StepThree>
+          <Step>
+            <h3>
+              <span>03</span>
+              O que eu vou levar
+              <br />
+              desse evento?
+            </h3>
+            <p>
+              Aprendizado prático de novas ferramentas, hacks para sua carreira
+              e um trabalho incrível para colocar no seu portfólio.
+            </p>
+            <p>
+              Lembre que certificados não acrescentam nada na sua carreira,
+              <b>
+                a coisa mais importante que você pode levar daqui é o
+                conhecimento.
+              </b>
+            </p>
+          </Step>
+        </StepThree>
+      </StepByStep>
     </Content>
   </Container>
 );
--- src/components/Description/styles.ts
+++ src/components/Description/styles.ts
@@ -1,5 +1,117 @@
 import styled from 'styled-components';
 
-export const Container = styled.section``;
+import icon3Img from '../../assets/icon3.svg';
+import icon4Img from '../../assets/icon4.svg';
 
-export const Content = styled.div``;
+export const Container = styled.section`
+  background-color: #121214;
+  padding-top: 222px;
+`;
+
+export const Content = styled.div`
+  max-width: 1160px;
+  width: 100%;
+  margin: 0 auto;
+  padding: 0 20px;
+`;
+
+export const Information = styled.div`
+  padding-bottom: 80px;
+
+  div:first-of-type {
+    max-width: 448px;
+
+    h2 {
+      font-size: 48px;
+      color: #e1e1e6;
+    }
+
+    p {
+      font-size: 18px;
+      color: #a8a8b3;
+      line-height: 1.5;
+      margin-top: 40px;
+    }
+  }
+
+  div:not(:first-of-type) {
+    max-width: 402px;
+    margin-left: auto;
+    position: relative;
+
+    p {
+      font-size: 36px;
+      color: #7159c1;
+      line-height: 1.5;
+      z-index: 2;
+
+      b {
+        color: #04d361;
+        font-weight: normal;
+      }
+    }
+
+    &::after {
+      content: '';
+      position: absolute;
+      top: -35%;
+      right: -15%;
+      width: 555px;
+      height: 555px;
+      background: url(${icon4Img}) center no-repeat;
+    }
+  }
+`;
+
+export const StepByStep = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const StepOne = styled.div``;
+
+export const StepTwo = styled.div`
+  margin-top: -125px;
+  margin-left: auto;
+`;
+
+export const StepThree = styled.div``;
+
+export const Step = styled.div`
+  position: relative;
+  z-index: 2;
+  max-width: 505px;
+  padding-left: 96px;
+
+  h3 {
+    font-size: 36px;
+    color: #e1e1e6;
+
+    span {
+      position: absolute;
+      font-weight: 700;
+      top: -71px;
+      left: 0;
+      font-size: 192px;
+      z-index: -1;
+      color: #202024;
+    }
+  }
+
+  p {
+    font-size: 18px;
+    line-height: 1.5;
+    padding-top: 40px;
+
+    b {
+      color: #04d361;
+    }
+  }
+
+  p:not(:last-child) {
+    padding-bottom: 80px;
+    border-bottom-width: 4px;
+    border-bottom-style: solid;
+    border-bottom-color: #7159c1;
+  }
+`;
