87a0f1c157c9b799d073b4145224370b20a5b520
João Victor Pereir Santos <joaovictorsantoscpv@gmail.com>
2020-05-26 12:00:19 -0300

    add: create structure of Disclaimer without style

--- src/components/Disclaimer/index.tsx
+++ src/components/Disclaimer/index.tsx
@@ -1,5 +1,37 @@
 import React from 'react';
 
-const Disclaimer: React.FC = () => <h1>Disclaimer</h1>;
+import { Container, Content } from './styles';
+
+import disclaimerImg from '../../assets/disclaimer.svg';
+import disclaimer2Img from '../../assets/disclaimer2Img.svg';
+
+const Disclaimer: React.FC = () => (
+  <Container>
+    <Content>
+      <img src={disclaimerImg} alt="Disclaimer" />
+      <div>
+        <h2>
+          Disclaimer
+          <p>
+            <b>
+              Ao final desse evento nós abrimos as inscrições para turmas dos
+              nossos bootcamps pagos.
+            </b>
+          </p>
+          <img src={disclaimer2Img} alt="Disclaimer" />
+          <p>
+            Mas o NLW não é uma amostra ou parte dos bootcamps: o conteúdo desse
+            evento é completo, inédito e exclusivo.
+          </p>
+          <p>
+            O NLW existe para entregar o que a gente tem de melhor para toda a
+            comunidade antes de colocar toda a nossa energia e foco nos nossos
+            alunos.
+          </p>
+        </h2>
+      </div>
+    </Content>
+  </Container>
+);
 
 export default Disclaimer;
