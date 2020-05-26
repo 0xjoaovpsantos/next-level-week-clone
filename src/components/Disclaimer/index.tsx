ac1a74d4b9a832213fe85c22eb6bc18018212c7c
João Victor Pereir Santos <joaovictorsantoscpv@gmail.com>
2020-05-26 12:13:51 -0300

    add: stylize structure of Disclaimer and make it responsive

--- src/components/Disclaimer/index.tsx
+++ src/components/Disclaimer/index.tsx
@@ -10,25 +10,23 @@ const Disclaimer: React.FC = () => (
     <Content>
       <img src={disclaimerImg} alt="Disclaimer" />
       <div>
-        <h2>
-          Disclaimer
-          <p>
-            <b>
-              Ao final desse evento nós abrimos as inscrições para turmas dos
-              nossos bootcamps pagos.
-            </b>
-          </p>
-          <img src={disclaimer2Img} alt="Disclaimer" />
-          <p>
-            Mas o NLW não é uma amostra ou parte dos bootcamps: o conteúdo desse
-            evento é completo, inédito e exclusivo.
-          </p>
-          <p>
-            O NLW existe para entregar o que a gente tem de melhor para toda a
-            comunidade antes de colocar toda a nossa energia e foco nos nossos
-            alunos.
-          </p>
-        </h2>
+        <h2>Disclaimer</h2>
+        <p>
+          <b>
+            Ao final desse evento nós abrimos as inscrições para turmas dos
+            nossos bootcamps pagos.
+          </b>
+        </p>
+        <img src={disclaimer2Img} alt="Disclaimer" />
+        <p>
+          Mas o NLW não é uma amostra ou parte dos bootcamps: o conteúdo desse
+          evento é completo, inédito e exclusivo.
+        </p>
+        <p>
+          O NLW existe para entregar o que a gente tem de melhor para toda a
+          comunidade antes de colocar toda a nossa energia e foco nos nossos
+          alunos.
+        </p>
       </div>
     </Content>
   </Container>
--- src/components/Disclaimer/styles.ts
+++ src/components/Disclaimer/styles.ts
@@ -1,5 +1,74 @@
 import styled from 'styled-components';
 
-export const Container = styled.section``;
+export const Container = styled.section`
+  background-color: #121214;
+  margin-top: 320px;
+  margin-bottom: 320px;
+`;
 
-export const Content = styled.div``;
+export const Content = styled.div`
+  max-width: 1160px;
+  width: 100%;
+  margin: 0 auto;
+  padding: 0 20px;
+
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  @media only screen and (max-width: 1030px) {
+    justify-content: center;
+  }
+
+  > img {
+    max-width: 577px;
+    width: 100%;
+    height: 100%;
+
+    @media only screen and (max-width: 1030px) {
+      display: none;
+    }
+  }
+
+  div {
+    max-width: 420px;
+
+    h2 {
+      font-size: 48px;
+      color: #e1e1e6;
+      padding-bottom: 40px;
+    }
+
+    p {
+      font-size: 18px;
+      line-height: 1.5;
+
+      @media only screen and (max-width: 640px) {
+        font-size: 15px;
+      }
+
+      b {
+        font-size: 24px;
+        color: #04d361;
+        font-weight: normal;
+      }
+    }
+
+    p:not(:last-child) {
+      padding-bottom: 40px;
+    }
+
+    img {
+      display: none;
+
+      @media only screen and (max-width: 1030px) {
+        display: block;
+        max-width: 577px;
+        height: 100%;
+        width: 100%;
+        margin-top: 40px;
+        margin-bottom: 80px;
+      }
+    }
+  }
+`;
